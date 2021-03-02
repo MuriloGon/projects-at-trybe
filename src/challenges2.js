69f96b5fdda4769a928de9002a5793d02cf06c2b
Murilo Gonçalves <murilo.gon@live.com>
2021-03-02 18:58:50 -0300

    refactor(requirement 10): change null to 'Vazio'

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,7 +2,7 @@
 function techList(techArr, nameStr) {
   let outputObj = [];
 
-  if (techArr.length <= 0) return null;
+  if (techArr.length <= 0) return 'Vazio!';
 
   for (let techItem of techArr.sort()) {
     outputObj.push(
