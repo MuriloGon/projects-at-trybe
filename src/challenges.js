4aaa70672768874ec7760b573c21248c30275591
Murilo Gonçalves <murilo.gon@live.com>
2021-03-02 16:13:27 -0300

    feat(requirement 09): :rocket: create a function to code and encode a string

--- src/challenges.js
+++ src/challenges.js
@@ -88,11 +88,42 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let word = '';
+  let vowels = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  for (let letterIndex = 0; letterIndex < phrase.length; letterIndex += 1) {
+    if (vowels[phrase[letterIndex].toLowerCase()]) {
+      word += vowels[phrase[letterIndex]];
+    } else {
+      word += phrase[letterIndex];
+    }
+  }
+  return word;
 }
-function decode() {
-  // seu código aqui
+
+function decode(phrase) {
+  let word = '';
+  let vowels = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  for (let letterIndex = 0; letterIndex < phrase.length; letterIndex += 1) {
+    if (vowels[phrase[letterIndex].toLowerCase()]) {
+      word += vowels[phrase[letterIndex]];
+    } else {
+      word += phrase[letterIndex];
+    }
+  }
+  return word;
 }
 
 module.exports = {
