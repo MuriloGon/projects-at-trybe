ebd7678348615aeffa159e931d048176feb2e931
Murilo Gonçalves <murilo.gon@live.com>
2021-03-13 19:20:18 -0300

    feat(requirement 03, 04): :rocket: add n colors circles dinamically

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function setRandomNumber(targetElement, colorRgb) {
   const tEl = targetElement;
   tEl.innerText = colorRgb;
 }
+function addCircles(parentElement, quantity) {
+  const n = quantity;
+  for (let i = 0; i < n; i += 1) {
+    const div = document.createElement('div');
+    div.classList.add('ball');
+    div.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}deg, 100%, 50%)`;
+    parentElement.appendChild(div);
+  }
+}
 
 const mainColorElement = document.getElementById('rgb-color');
+const colorCirclesElement = document.getElementById('color-circles');
 
 setRandomNumber(mainColorElement, 'rgb(120, 120, 120)');
+addCircles(colorCirclesElement, 6);
