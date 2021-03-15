96c28b8bb07222b8e6cf474ea94900fd8f1f6683
Murilo Gonçalves <murilo.gon@live.com>
2021-03-14 21:46:10 -0300

    feat(requirement 06 to 18): :rocket: add interdependent requirements at once
    It's not possible to commit step by step 'cause cypress can't asses correctly the requirements

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function addLetter(inpuElement, pElement) {
+function addRandomClasses(classesObject, spanElement) {
+  const groupsKeys = Object.keys(classesObject);
+  const groupsLen = groupsKeys.length;
+
+  for (let i = 0; i < groupsLen; i += 1) {
+    const groupClasses = classesObject[groupsKeys[i]];
+    const groupClassesLen = groupClasses.length; // (+num) increase the none style probability
+    const randomClassIndex = Math.round(Math.random() * groupClassesLen);
+
+    if (groupClasses[randomClassIndex] !== undefined) {
+      spanElement.classList.add(groupClasses[randomClassIndex]);
+    }
+  }
+}
+
+function addLetter(inpuElement, pLetterElement, groupsClasses) {
   const letterWords = inpuElement.value.split(' ');
-  const pEl = pElement;
+  const pEl = pLetterElement;
   pEl.innerText = '';
 
   for (let i = 0; i < letterWords.length; i += 1) {
@@ -9,13 +24,14 @@ function addLetter(inpuElement, pElement) {
 
     if (word !== '') {
       spanEl.innerText = letterWords[i];
+      addRandomClasses(groupsClasses, spanEl);
       pEl.appendChild(spanEl);
     }
   }
 }
 
-function nullLetter(pElement) {
-  const pEl = pElement;
+function nullLetter(pLetterElement) {
+  const pEl = pLetterElement;
   pEl.innerText = 'Por favor, digite o conteúdo da carta.';
 }
 
@@ -24,22 +40,41 @@ function isNull(inputElement) {
   let letters = 0;
   for (let i = 0; i < inputEl.value.length; i += 1) {
     if (inputEl.value[i] !== ' ') letters += 1;
-    console.log(letters);
   }
   if (letters > 0) return false;
   return true;
 }
 
-function generateLetter(btnGenerateLetter, inputElement, pElement) {
+function countWords(pWordCountElement, pLetteElement) {
+  const pCountEl = pWordCountElement;
+  const words = pLetteElement.children.length;
+  pCountEl.innerText = `${words}`;
+}
+
+function generateLetter(btnGenerateLetter, inputElement, groupClasses, pElObjs) {
   btnGenerateLetter.addEventListener('click', () => {
     const inputEl = inputElement;
-    const pEl = pElement;
-    if (inputEl.value !== '' && !isNull(inputEl)) addLetter(inputEl, pEl);
-    else nullLetter(pEl);
+    const pLetterEl = pElObjs.pLetterElement;
+    const pCountEl = pElObjs.pCountElement;
+    if (inputEl.value !== '' && !isNull(inputEl)) {
+      addLetter(inputEl, pLetterEl, groupClasses);
+    } else nullLetter(pLetterEl);
+    countWords(pCountEl, pLetterEl);
   });
 }
 
+const letterGroups = {
+  style: ['newspaper', 'magazine1', 'magazine2'],
+  size: ['medium', 'big', 'reallybig'],
+  rotate: ['rotateleft', 'rotateright'],
+  skew: ['skewleft', 'skewright'],
+};
+
+const pElementsObj = {
+  pLetterElement: document.getElementById('carta-gerada'),
+  pCountElement: document.getElementById('carta-contador'),
+};
+
 const btnGenLetter = document.getElementById('criar-carta');
 const inputElement = document.getElementById('carta-texto');
-const pElement = document.getElementById('carta-gerada');
-generateLetter(btnGenLetter, inputElement, pElement);
+generateLetter(btnGenLetter, inputElement, letterGroups, pElementsObj);
