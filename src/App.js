58915c3d0177626200428b16aa143d0412a5c090
Murilo Gonçalves <murilo.gon@live.com>
2021-03-22 04:15:20 -0300

    feat(requirement 03): :rocket: add MovieList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+
+const data = [{
+  title: 'Título 1',
+  subtitle: 'SubTitle 1',
+  description: 'Description 1',
+  rating: 4.5,
+  img: 'http://img.jpg',
+}];
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ data } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,31 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+export default class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    console.log('movies', movies);
+    return (
+      <div>
+        {movies.map((movie) => {
+          console.log(movie);
+          return (
+            <MovieCard key={ movie.title.split(' ').join('-') } movie={ movie } />);
+        })}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape(
+    {
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+      img: PropTypes.string.isRequired,
+    },
+  )).isRequired,
+};
