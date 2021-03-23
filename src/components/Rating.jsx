80b406c4a878441d885bc206d30c4e4359930e09
Murilo Gonçalves <murilo.gon@live.com>
2021-03-23 01:33:10 -0300

    feat(requirement 11): :rocket: add rating component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,14 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+class Rating extends React.Component {
+  render() {
+    const { rate } = this.props;
+    return (
+      <span>{rate}</span>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rate: PropTypes.number.isRequired,
+};
