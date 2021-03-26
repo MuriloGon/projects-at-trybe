e0d78cebd300f69e5c0910491242cde858b6cecb
Murilo Gonçalves <murilo.gon@live.com>
2021-03-26 12:57:36 -0300

    feat(createStudent): 🚀 implement createStudent function

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => ({
+  name: name,
+  feedback: () => 'Eita pessoa boa!'
+});
 
 module.exports = createStudent;
