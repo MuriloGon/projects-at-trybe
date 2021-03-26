3cb67cceda0e64b13baed8d5211e1b4bb5ec3fad
Murilo Gonçalves <murilo.gon@live.com>
2021-03-26 12:39:19 -0300

    feat(vqv): 🚀 implement vqv function

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (!nome || !idade) return undefined;
+
+  return (
+    `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`);
+};
 
 module.exports = vqv;
