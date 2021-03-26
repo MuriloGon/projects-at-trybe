c85d3387359f563f365ccebec2bc498f92b45210
Murilo Gonçalves <murilo.gon@live.com>
2021-03-26 12:38:10 -0300

    feat(numbers): 🚀 implement numbers function

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,10 +18,14 @@ const numbers = require('../src/numbers');
 
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
-    assert.fail();
+    // assert.fail();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    assert.deepStrictEqual(numbers([1, 'a', 3]), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    assert.deepStrictEqual(numbers(['']), false);
   });
 });
