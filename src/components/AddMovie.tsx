b987756577132b683c923bb5af15548b994527fe
Murilo Gonçalves <murilo.gon@live.com>
2021-03-27 20:21:12 -0300

    feat(req06): :rocket: create the <AddMovie/>

--- src/components/AddMovie.tsx
+++ src/components/AddMovie.tsx
@@ -1,7 +1,24 @@
-import {Component} from 'react';
+import React, { Component } from 'react';
+import {IMovie} from './Interfaces';
+interface IProp {
+  onClick: ()=>void;
+}
 
-class AddMovie extends Component {
-
+class AddMovie extends Component<IProp, IMovie> {
+  constructor(props: IProp){
+    super(props);
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      rating: 0,
+      storyline: 'action',
+    }
+  }
+  
+  render(): JSX.Element {
+    return <h1>oi</h1>;
+  }
 }
 
 export default AddMovie;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { SearchBar } from './components/SearchBar';
+import { AddMovie } from './components/AddMovie';
 
 ReactDOM.render(
-  <SearchBar/>, document.getElementById('root'),
+  <AddMovie/>, document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
