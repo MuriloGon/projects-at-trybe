fd04aafb472414ca4795eee15abefce55f15283d
Murilo Gonçalves <murilo.gon@live.com>
2021-03-27 12:52:52 -0300

    feat(req03): :rocket: render a forn in <SearchBar/>

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,17 +1,27 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 
-interface IProps{
+interface Props {
   searchText: string;
-  onSearchTextChange: ()=>void;
+  onSearchTextChange: () => void;
   bookmarkedOnly: boolean;
   onBookmarkedChange: () => void;
   selectedGenre: string;
-  onSelectedGenreChange: ()=>void;
+  onSelectedGenreChange: () => void;
 }
-class SearchBar extends React.Component<IProps> {
-  render() {
+class SearchBar extends React.Component<Props> {
+  render(): React.ReactElement {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">Inclui o texto:</label>
+        <input
+          type="text"
+          data-testid="text-input"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
       </form>
     );
   }
