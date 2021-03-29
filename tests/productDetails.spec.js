0022af2794732f48d46261cab4364795f7c2f60e
Murilo Gonçalves <murilo.gon@live.com>
2021-03-29 15:16:37 -0300

    test(productDetails): :bug: fix tests

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -41,14 +41,17 @@ describe('#productDetails', () => {
     const out = productDetails('Alcool gel', 'Máscara');
     assert.deepStrictEqual(out.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.deepStrictEqual(typeof (out[0]) === 'object' && typeof (out[1]) === 'object', true);
+    assert.deepStrictEqual(typeof out[0], 'object')
+    assert.deepStrictEqual(typeof out[1], 'object')
+    assert.deepStrictEqual(typeof out[0], typeof out[1]);
     // Teste que os dois objetos são diferentes entre si.
     assert.notDeepStrictEqual(out[0], out[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
     const obj1 = out[0].details.productId;
     const obj2 = out[1].details.productId;
-    const str1 = obj1.substr(obj1.length - 4, obj1 - 1);
-    const str2 = obj2.substr(obj2.length - 4, obj2 - 1);
-    assert.deepStrictEqual(str1 === str2, true);
+    const str1 = obj1.substr(-3); //sugestão do lucas godoi
+    const str2 = obj2.substr(-3); //sugestão do lucas godoi
+    assert.deepStrictEqual(str1, str2);
+    assert.notDeepStrictEqual(str1, str2.substr(-2));
   });
 });
