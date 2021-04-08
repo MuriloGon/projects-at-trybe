e7c6ddebfe4cb4f491d63a34bdb5f425b275d1cb
Murilo Gonçalves <murilo.gon@live.com>
2021-04-08 20:23:31 -0300

    feat(req01): :rocket: implement animalsByIds function
    the function uses: rest parms, filter and include methods

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,9 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  const { animals } = data;
+  return animals.filter(({ id }) => ids.includes(id));
 }
 
 function animalsOlderThan(animal, age) {
