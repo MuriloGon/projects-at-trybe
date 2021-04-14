ca7482dbdc483610fc891ec61a63f045e2aac7ac
Murilo Gonçalves <murilo.gon@live.com>
2021-04-14 15:44:33 -0300

    feat(req02): 🚀 implement add item to cart behaviour

--- script.js
+++ script.js
@@ -12,20 +12,25 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ sku, name, image, price }) {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement('span', 'item__price', price));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+function getParametersFromItem(item) {
+  return {
+    sku: item.querySelector('span.item__sku').innerText,
+    name: item.querySelector('span.item__title').innerText,
+    salePrice: item.querySelector('span.item__price').innerText,
+  };
 }
 
 function cartItemClickListener(event) {
@@ -40,6 +45,8 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+/* #### Create the main list of products #### */
+// Async Queries functions
 async function fetchQueryProducts(QUERY) {
   const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
   const response = await fetch(URL);
@@ -47,16 +54,34 @@ async function fetchQueryProducts(QUERY) {
   return data;
 }
 
+// Populate querie items on the page
 async function createListItems(itemsQuerySelector, productQuery) {
   const { results } = await fetchQueryProducts(productQuery);
+  console.log(results);
   const container = document.querySelector(itemsQuerySelector);
   container.innerHTML = '';
 
-  results.forEach(({ id: sku, title: name, thumbnail: image }) => {
-    container.appendChild(createProductItemElement({ sku, name, image }));
+  results.forEach(({ id: sku, title: name, thumbnail: image, price }) => {
+    container.appendChild(createProductItemElement({ sku, name, image, price }));
+  });
+}
+
+// Add item to cart list
+async function addToCartEvt(querySelector) {
+  const items = document.querySelector(querySelector);
+  items.addEventListener('click', (evt) => {
+    const cartListEl = document.querySelector('.cart__items');
+    const { target } = evt;
+
+    if (target.classList.contains('item__add')) {
+      const { parentElement } = target;
+      const parms = getParametersFromItem(parentElement);
+      cartListEl.appendChild(createCartItemElement(parms));
+    }
   });
 }
 
 window.onload = function onload() {
   createListItems('.items', 'computador');
+  addToCartEvt('.items');
 };
