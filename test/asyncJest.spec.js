ba3ce4b673e67c9d319f33078b3add01f8a8b5a0
Murilo Gonçalves <murilo.gon@live.com>
2021-04-23 13:34:15 -0300

    test(req01): :white_check_mark: async jest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,4 @@
-const assert = require('assert');
-const answerPhone = require('../src/asyncJest');
+let answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -10,13 +9,28 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+answerPhone = jest.fn((test) =>
+  (test
+    ? Promise.resolve('Oi!')
+    : Promise.reject(new Error('Infelizmente não podemos atender...'))));
+
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    expect.assertions(3);
+
+    const response = await answerPhone(true);
+
+    expect(answerPhone).toBeDefined();
+    expect(answerPhone).toBeCalledTimes(1);
+    expect(response).toMatch('Oi');
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async () => {
+    expect.assertions(2);
+    try {
+      await answerPhone(false);
+    } catch (error) {
+      expect(error).toEqual(Error('Infelizmente não podemos atender...'));
+      expect(answerPhone).toBeCalledTimes(2);
+    }
   });
 });
