8bd1b7dc08ad34e914c1f1f2d09a1d1afc6ae654
Murilo Gonçalves <murilo.gon@live.com>
2021-05-03 16:05:52 -0300

    feat(req16): 🚀 add initial state to MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const MovieLibrary = () => (<div>MovieLibrary</div>);
+const MovieLibrary = ({ movies: moviesProps }) => {
+  const [searchText, setSearchText] = useState('');
+  const [bookmarkedOnly, setBookmarkedOnly] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState('');
+  const [movies] = useState(moviesProps);
+
+  return (
+    <h1>MovieLibrary</h1>
+  );
+};
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export { MovieLibrary as default };
