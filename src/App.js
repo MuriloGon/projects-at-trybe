2d92f420b2d405475f11dda39a3e28fe5ba85b9e
Murilo Gonçalves <murilo.gon@live.com>
2021-05-03 17:04:03 -0300

    feat(req18): 🚀 render <MovieList /> in <MovieLibrary />

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary movies={ data } />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,11 +4,28 @@ import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 
+const filteredMovies = (movies, text, isFavorite, selectedGenre) => {
+  let filtered = [...movies];
+
+  filtered = filtered.filter(({ title, subtitle, storyline }) => (
+    title.toUpperCase().includes(text.toUpperCase())
+  || subtitle.toUpperCase().includes(text.toUpperCase())
+  || storyline.toUpperCase().includes(text.toUpperCase())));
+
+  filtered = filtered.filter(({ bookmarked }) => (isFavorite
+    ? (bookmarked && isFavorite) : true));
+
+  filtered = filtered.filter(({ genre }) => (selectedGenre === ''
+    ? true : (selectedGenre === genre)));
+
+  return filtered;
+};
+
 const MovieLibrary = ({ movies: moviesProps }) => {
   const [searchText, setSearchText] = useState('');
   const [bookmarkedOnly, setBookmarkedOnly] = useState(false);
   const [selectedGenre, setSelectedGenre] = useState('');
-  const [movies] = useState(moviesProps);
+  const movies = filteredMovies(moviesProps, searchText, bookmarkedOnly, selectedGenre);
 
   return (
     <>
