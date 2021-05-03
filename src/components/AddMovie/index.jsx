d5924a6c9b91e5a16a20eb0cadc638d5a2aea709
Murilo Gonçalves <murilo.gon@live.com>
2021-05-03 14:58:22 -0300

    feat(req08): 🚀 render title input in <MovieCard/>
    Render a text input inside the form in <AddMovie /> to get the title of the new movie

--- src/components/AddMovie/index.jsx
+++ src/components/AddMovie/index.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddMovie = () => (
-  <form data-testid="add-movie-form" />
-);
+const AddMovie = () => {
+  const [title, setTitle] = useState('');
+
+  return (
+    <form data-testid="add-movie-form">
+      <label data-testid="title-input-label" htmlFor="title-input">
+        Título
+        <input
+          id="title-input"
+          type="text"
+          value={ title }
+          data-testid="title-input"
+          onChange={ ({ target: { value } }) => setTitle(value) }
+        />
+      </label>
+    </form>
+  );
+};
 
 export { AddMovie as default };
