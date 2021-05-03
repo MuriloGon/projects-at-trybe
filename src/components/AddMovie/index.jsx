2ce260102b43b8a9a689b1d65de175b178fb1572
Murilo Gonçalves <murilo.gon@live.com>
2021-05-03 15:43:17 -0300

    feat(req14): 🚀 render submit button in <AddMovie/>
    Render a button of the form in <AddMovie /> to make use of the data of the new film, contained in the state of <AddMovie />

--- src/components/AddMovie/index.jsx
+++ src/components/AddMovie/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 import TitleInput from './TitleInput';
 import SubtitleInput from './SubtitleInput';
@@ -7,7 +8,18 @@ import StorylineInput from './StorylineInput';
 import RatingInput from './RatingInput';
 import GenreInput from './GenreInput';
 
-const AddMovie = () => {
+const resetState = ({
+  setTitle, setSubtitle, setImagePath,
+  setStoryline, setRating, setGenre }) => {
+  setTitle('');
+  setSubtitle('');
+  setImagePath('');
+  setStoryline('');
+  setRating(0);
+  setGenre('Action');
+};
+
+const AddMovie = ({ onClick }) => {
   const [title, setTitle] = useState('');
   const [subtitle, setSubtitle] = useState('');
   const [imagePath, setImagePath] = useState('');
@@ -15,16 +27,39 @@ const AddMovie = () => {
   const [rating, setRating] = useState(0);
   const [genre, setGenre] = useState('Action');
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onClick({
+      title,
+      subtitle,
+      imagePath,
+      rating,
+      storyline,
+      genre,
+    });
+    resetState({ setTitle,
+      setSubtitle,
+      setImagePath,
+      setStoryline,
+      setRating,
+      setGenre });
+  };
+
   return (
-    <form data-testid="add-movie-form">
+    <form data-testid="add-movie-form" onSubmit={ handleSubmit }>
       <TitleInput { ...{ title, setTitle } } />
       <SubtitleInput { ...{ subtitle, setSubtitle } } />
       <ImageInput { ...{ imagePath, setImagePath } } />
       <StorylineInput { ...{ storyline, setStoryline } } />
       <RatingInput { ...{ rating, setRating } } />
       <GenreInput { ...{ genre, setGenre } } />
+      <button data-testid="send-button" type="submit">Adicionar filme</button>
     </form>
   );
+};
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
 };
 
 export { AddMovie as default };
