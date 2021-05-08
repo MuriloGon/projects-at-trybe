9c5e779c1d2669b4f9508a94b14982484d3a8d78
Murilo Gonçalves <murilo.gon@live.com>
2021-05-08 20:42:59 -0300

    feat(req04): :sparkles: implement MovieDetails
    data fetched from mocke api and loading indicator

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,42 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams, useRouteMatch } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
-class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+const fetchMovie = async (id, setLoading, setMovie) => {
+  const movie = await movieAPI.getMovie(id);
+  setLoading(false);
+  setMovie(movie);
+};
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+const MovieDetails = () => {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
+  const { id } = useParams();
+  const { url } = useRouteMatch();
 
-    return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-      </div>
-    );
-  }
-}
+  const { title, storyline, imagePath,
+    genre, rating, subtitle } = movie;
+
+  useEffect(() => {
+    fetchMovie(id, setLoading, setMovie);
+  }, [id]);
+
+  return loading ? <Loading /> : (
+    <div data-testid="movie-details">
+      <img alt="Movie Cover" src={ `../${imagePath}` } />
+      <p>{ `Subtitle: ${title}` }</p>
+      <p>{ `Subtitle: ${subtitle}` }</p>
+      <p>{ `Storyline: ${storyline}` }</p>
+      <p>{ `Genre: ${genre}` }</p>
+      <p>{ `Rating: ${rating}` }</p>
+
+      <Link to={ `${url}/edit` }>EDITAR</Link>
+      <Link to="/">DELETAR</Link>
+      <Link to="/">VOLTAR</Link>
+    </div>
+  );
+};
 
 export default MovieDetails;
