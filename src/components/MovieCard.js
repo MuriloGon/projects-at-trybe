b9878a71e286c0328037a4ebd3e5762e4108f1c6
Murilo Gonçalves <murilo.gon@live.com>
2021-05-08 19:36:29 -0300

    feat(req03): :rocket: implement MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-class MovieCard extends React.Component {
-  render() {
-    return (
-      <div data-testid="movie-card">
-        Movie Card
-      </div>
-    );
-  }
-}
+const MovieCard = ({ movie: { id, title, subtitle, storyline } }) => (
+  <div data-testid="movie-card">
+    <h2>{title}</h2>
+    <h3>{subtitle}</h3>
+    <h1>{storyline}</h1>
+    <Link to={ `movies/${id}` }>VER DETALHES</Link>
+  </div>
+);
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape(
+    {
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      storyline: PropTypes.string.isRequired,
+    },
+  ).isRequired,
+};
 
 export default MovieCard;
