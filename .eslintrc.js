a6d6bc3e0debf5e1de748ca32637a5027cac1aed
Murilo Gonçalves <murilo.gon@live.com>
2021-05-20 17:48:50 -0300

    build(depedencies): :heavy_plus_sign: add eslint devdepedencie

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+};
