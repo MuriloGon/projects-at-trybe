097d997ac2d0400a1d8b954a1651ba9068945e6c
Murilo Gonçalves <murilo.gon@live.com>
2021-06-12 05:55:19 -0300

    feat(req01,02,03): :sparkles: implement login components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect, Route, Switch } from 'react-router';
+import { Link } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
-}
+const handleAuth = (bool, component, to = '/not-allowed') => (
+  bool ? component : <Redirect to={ `${to}` } />
+);
 
-export default App;
+const App = ({ isLogged }) => (
+  <Switch>
+    <Route exact path="/">
+      <Login />
+    </Route>
+
+    <Route exact path="/login">
+      <Login />
+    </Route>
+
+    <Route exact path="/carteira">
+      {handleAuth(isLogged, <Wallet />)}
+    </Route>
+
+    <Route path="/not-allowed">
+      <div style={ { textAlign: 'center' } }>
+        <h1>Logue-se para acessar à aplicação</h1>
+        <Link to="/login">Faça o login aqui</Link>
+      </div>
+    </Route>
+
+    <Route path="*">
+      <h1>ERROR 404</h1>
+    </Route>
+
+  </Switch>
+);
+
+App.propTypes = {
+  isLogged: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  isLogged: state.auth,
+});
+
+export default connect(mapStateToProps, null)(App);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+import './styles/Login.css';
 
-export default Login;
+import { Redirect } from 'react-router';
+import { login, saveEmail as saveEmailStore } from '../actions';
+import placeholder from '../assets/login_placeholder.svg';
+
+const MAX_LENGTH = 6;
+
+const btnStyle = (validation) => ({
+  cursor: validation ? 'pointer' : 'not-allowed',
+});
+
+const Login = ({ authLogin, saveEmail, isLogged }) => {
+  const [loginEmail, setUser] = useState('');
+  const [loginPassword, setPassword] = useState('');
+
+  const validatelogin = (loginEmail === 'alguem@email.com')
+    && (loginPassword.length >= MAX_LENGTH);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    authLogin();
+    saveEmail(loginEmail);
+  };
+
+  const loggedRender = <Redirect to="/carteira" />;
+
+  const defaultRender = (
+    <form className="login-form">
+      <img className="login-form-img" src={ placeholder } alt="login placeholder" />
+      <input
+        autoComplete="on"
+        className="login-form-input"
+        value={ loginEmail }
+        onChange={ ({ target: { value } }) => setUser(value) }
+        placeholder="Email"
+        type="email"
+        data-testid="email-input"
+      />
+      <input
+        autoComplete="on"
+        className="login-form-input"
+        value={ loginPassword }
+        onChange={ ({ target: { value } }) => setPassword(value) }
+        placeholder="Password"
+        type="password"
+        data-testid="password-input"
+      />
+      <button
+        style={ btnStyle(validatelogin) }
+        onClick={ handleSubmit }
+        className="login-form-submit"
+        type="submit"
+        disabled={ !validatelogin }
+      >
+        Entrar
+      </button>
+    </form>
+  );
+
+  return isLogged ? loggedRender : defaultRender;
+};
+
+const mapStateToProps = (state) => ({
+  isLogged: state.auth,
+});
+const mapDispatchToProps = (dispatch) => ({
+  authLogin: () => dispatch(login()),
+  saveEmail: (email) => dispatch(saveEmailStore(email)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
