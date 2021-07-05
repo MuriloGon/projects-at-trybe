bc86475737e520be72f59a6d97cf44c0b420bde4
MaxRudim <max.hermsdorff@gmail.com>
2021-07-05 15:11:06 -0300

    feat(header): :sparkles: add Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Header = ({ name, renderSearchButton }) => (
-  <h1>
-    {name}
-    {' '}
-    {renderSearchButton && <span>searchButton</span>}
-  </h1>
+  <div>
+    <Link to="/perfil">
+      <img
+        src="src/images/profileIcon.svg"
+        alt="Profile Icon"
+        data-testid="profile-top-btn"
+      />
+      <h1 data-testid="page-title">{name}</h1>
+    </Link>
+    <input
+      type="image"
+      src="src/images/searchIcon.svg"
+      alt="Search Icon"
+      data-testid="search-top-btn"
+    />
+  </div>
+  // <h1>
+  //   {name}
+  //   {' '}
+  //   {renderSearchButton && <span>searchButton</span>}
+  // </h1>
 );
 
 Header.propTypes = {
