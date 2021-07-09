7b7d22de889ea130dc1f833ca43c3801282ca5ab
MaxRudim <max.hermsdorff@gmail.com>
2021-07-09 15:45:25 -0300

    feat(Footer): :sparkles: add buttons and icons
    add buttons for the footer component, still lacks CSS customization and display on certain routes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
-const Footer = () => <h1>footer</h1>;
+function Footer() {
+  return (
+    <footer data-testid="footer">
+      <Link to="/bebidas">
+        <img
+          src={ drinkIcon }
+          alt="Drink Icon"
+          data-testid="drinks-bottom-btn"
+        />
+      </Link>
+      <Link to="/explorar">
+        <img
+          src={ exploreIcon }
+          alt="Explore Icon"
+          data-testid="explore-bottom-btn"
+        />
+      </Link>
+      <Link to="/comidas">
+        <img
+          src={ mealIcon }
+          alt="Food Icon"
+          data-testid="food-bottom-btn"
+        />
+      </Link>
+    </footer>
+  );
+}
 
 export default Footer;
