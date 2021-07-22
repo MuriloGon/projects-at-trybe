b24ef412b7d1901e494a8433ab69629cd2f98df3
Murilo Gonçalves <murilo.gon@live.com>
2021-07-22 15:04:51 -0300

    feat(challenge06): ✨ finish challenge 06

---
+++ desafio6.sql
@@ -0,0 +1,3 @@
+-- Exiba os dados das colunas product_name e id da tabela products de maneira que os resultados estejam em ordem alfabética dos nomes.
+SELECT id, product_name FROM northwind.products
+ORDER BY product_name ASC;
