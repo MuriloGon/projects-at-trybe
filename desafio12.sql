087f70bbe21e6172af64705a047f2bb0d29386a0
Murilo Gonçalves <murilo.gon@live.com>
2021-07-22 15:05:44 -0300

    feat(challenge12): ✨ finish challenge 12

---
+++ desafio12.sql
@@ -0,0 +1,3 @@
+-- Mostre as submitted_date de purchase_orders em que a submitted_date é do dia 26 de abril de 2006.
+SELECT submitted_date FROM northwind.purchase_orders
+WHERE DATE(submitted_date) = '2006-04-26';
