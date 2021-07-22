06d098fc71dbd5340133447268047ae33171d0b5
Murilo Gonçalves <murilo.gon@live.com>
2021-07-22 15:06:12 -0300

    feat(challenge15): ✨ finish challenge 15

---
+++ desafio15.sql
@@ -0,0 +1,2 @@
+-- Mostre somente as horas (sem os minutos e os segundos) da submitted_date de todos registros de purchase_orders. Chame essa coluna de submitted_hour.
+SELECT HOUR(submitted_date) as submitted_hour FROM northwind.purchase_orders;
