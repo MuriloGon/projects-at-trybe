6295c9933a89323e0100369013ef0e033710cdd2
Murilo Gonçalves <murilo.gon@live.com>
2021-08-03 20:00:28 -0300

    feat(req08): finish challenge 08

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.users
+FOR EACH ROW
+BEGIN
+    DELETE FROM SpotifyClone.user_follows WHERE OLD.user_id = user_id;
+    DELETE FROM SpotifyClone.played_songs WHERE OLD.user_id = user_id;
+END $$
+
+DELIMITER $$ ;
