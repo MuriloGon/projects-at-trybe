d126788fec46a6ed71dab722995807431e3f419e
Murilo Gonçalves <murilo.gon@live.com>
2021-08-03 21:07:26 -0300

    refactor: remove spaces

--- desafio8.sql
+++ desafio8.sql
@@ -4,8 +4,8 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
-    DELETE FROM SpotifyClone.user_follows WHERE OLD.user_id = user_id;
-    DELETE FROM SpotifyClone.played_songs WHERE OLD.user_id = user_id;
+  DELETE FROM SpotifyClone.user_follows WHERE OLD.user_id = user_id;
+  DELETE FROM SpotifyClone.played_songs WHERE OLD.user_id = user_id;
 END $$
 
 DELIMITER $$ ;
