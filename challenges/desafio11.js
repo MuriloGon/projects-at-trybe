778fad9d7f48097c1fbaf0e669b02ff336fa3292
Murilo Gonçalves <murilo.gon@live.com>
2021-08-11 12:25:53 -0300

    feat(req11): :sparkles: finish challenge 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,8 @@
+// 11 - Retorne a quantidade de documentos em que o campo aeroportoDestino.pais não seja igual a ESTADOS UNIDOS.
+db.voos.find({
+  "aeroportoDestino.pais": {
+    $not: {
+      $eq: "ESTADOS UNIDOS",
+    },
+  },
+}).count();
