1d37685f4ae3d30de722ca32ce8b8146b5117ba6
Murilo Gonçalves <murilo.gon@live.com>
2021-08-11 12:33:02 -0300

    feat(req26): ✨ finish challenge 26

---
+++ challenges/desafio26.js
@@ -0,0 +1,8 @@
+// 26 - Remova todos os voos da empresa GOL em que a quantidade de passageiros pagos esteja entre 5 e 10, incluindo os casos em que a quantidade é 5 e 10. Informe a quantidade de documentos removidos.
+db.voos.deleteMany({
+  "empresa.nome": "GOL",
+  "passageiros.pagos": {
+    $gte: 5,
+    $lte: 10,
+  },
+});
