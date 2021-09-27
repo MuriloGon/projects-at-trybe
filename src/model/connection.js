3763a139af068f8228d41c452f2c7675e36f5f5f
Murilo Gonçalves <murilo.gon@live.com>
2021-09-27 04:11:30 -0300

    feat: add env variables on the db connection

--- src/model/connection.js
+++ src/model/connection.js
@@ -1,8 +1,10 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 let db = null;
 
-const uri = 'mongodb://localhost:27017/Cookmaster';
+const { DB_NAME = 'Cookmaster', MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster' } = process.env;
+
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -11,8 +13,8 @@ const options = {
 async function connection() {
   if (db) return db;
 
-  const conn = await MongoClient.connect(uri, options);
-  db = conn.db('Cookmaster');
+  const conn = await MongoClient.connect(MONGO_DB_URL, options);
+  db = conn.db(DB_NAME);
   return db;
 }
 
