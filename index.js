1d576ed472feec70e6e938b72dfa7f44fffd46ec
Murilo Gonçalves <murilo.gon@live.com>
2021-09-30 23:05:49 -0300

    feat: add env var port at index.js

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
