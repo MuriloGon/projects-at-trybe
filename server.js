730d1bbd3d86ef153fddfca2fd5c9ebefcc9109d
Murilo Gonçalves <murilo.gon@live.com>
2021-10-29 19:53:38 -0300

    feat: define basic sockets structure

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-// Faça seu código aqui
+const express = require('express');
+const Http = require('http');
+const { Server } = require('socket.io');
+const moment = require('moment');
+
+const app = express();
+const http = Http.createServer(app);
+app.use(express.static('public'));
+
+const io = new Server(http);
+
+io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    io.emit('message', `${moment().format('DD-MM-yyyy HH:mm:ss')} ${nickname} ${chatMessage}`);
+  });
+});
+
+const { PORT = 3000 } = process.env;
+http.listen(PORT, () => console.log(`listening ${PORT}`));
